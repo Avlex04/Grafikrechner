d8513a938b90aa44606cd6143f00faa28f164e9c
alex <alex@mail.com>
2023-02-27 10:19:14 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.grafikrechner {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.grafikrechner to javafx.fxml;
+    exports com.example.grafikrechner;
+}
