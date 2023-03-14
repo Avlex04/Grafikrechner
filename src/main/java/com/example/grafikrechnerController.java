4200604afb3b83c6c57b6b1b2df05c1344c1dd82
alex <alex@mail.com>
2023-03-14 13:15:20 +0100

    ja

--- src/main/java/com/example/grafikrechnerController.java
+++ src/main/java/com/example/grafikrechnerController.java
@@ -2,6 +2,11 @@ package com.example;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+
 
 public class grafikrechnerController {
     @FXML
@@ -18,6 +23,8 @@ public class grafikrechnerController {
     private TextField hoch0;
     @FXML
     private Label outputLabel;
+    @FXML
+    private  Canvas canvas;
 
 
     public void initialize() {
@@ -29,15 +36,26 @@ public class grafikrechnerController {
         String text4 = hoch2.getText();
         String text5 = hoch1.getText();
         String text6 = hoch0.getText();
+        intercept();
 
         if (text1.matches("-?\\d+(\\.\\d+)?") && text2.matches("-?\\d+(\\.\\d+)?") && text3.matches("-?\\d+(\\.\\d+)?") && text4.matches("-?\\d+(\\.\\d+)?") && text5.matches("-?\\d+(\\.\\d+)?") && text6.matches("-?\\d+(\\.\\d+)?")) {
 
-            outputLabel.setText(text1 + "x^5" + " + " + text2 + "x^4"+ " + " + text3 + "x^3"+ " + " + text4 + "x^2"+ " + " + text5 + "x" + " + " + text6 );
+            outputLabel.setText(text1 + "x^5" + " + " + text2 + "x^4" + " + " + text3 + "x^3" + " + " + text4 + "x^2" + " + " + text5 + "x" + " + " + text6);
             System.out.println("test");
         } else {
             // Display an error message on the label
             outputLabel.setText("Bitte in jedes Feld Nummern eingeben");
             System.out.println("test2");
         }
+    }
+    public void intercept() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(1);
+        gc.strokeLine(0, 155, 710, 155);
+
+        // Draw the Y-axis
+        gc.strokeLine(355, 350, 355, 0);
     }
 }
